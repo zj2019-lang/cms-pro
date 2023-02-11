2df7a0217282e563f9a4dfe8795e38cbf26f138b
zj2019-lang <1184826291@qq.com>
2023-02-11 19:18:12 +0800

    初始化,同步github

---
+++ cms-portal/src/main/java/com/cms/portal/security/filter/CmsAuthenticationFilter.java
@@ -0,0 +1,8 @@
+package com.cms.portal.security.filter;
+
+import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+
+public class CmsAuthenticationFilter  extends FormAuthenticationFilter {
+
+    
+}
