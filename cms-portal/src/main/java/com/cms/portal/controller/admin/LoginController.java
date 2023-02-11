83a74333b5a7fe1941d997e1e19748740bc55cc4
zj2019-lang <1184826291@qq.com>
2023-02-11 17:51:35 +0800

    初始化,GitHub

--- cms-portal/src/main/java/com/cms/portal/controller/admin/LoginController.java
+++ cms-portal/src/main/java/com/cms/portal/controller/admin/LoginController.java
@@ -1,6 +1,7 @@
 package com.cms.portal.controller.admin;
 
 import com.google.code.kaptcha.Producer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 @Controller
+@Slf4j
 public class LoginController {
 
     @Autowired
